49ddf40770da5afbb8998ce66cee80aab6194352
Molly Le <mollycule21@gmail.com>
2021-02-13 20:49:12 -0800

    regfile, reg not instantiated

--- Lab1/register_file.sv
+++ Lab1/register_file.sv
@@ -9,11 +9,16 @@
 module register_file (input logic [4:0] read_reg_1, read_reg_2, wr_reg, 
 						input logic [31:0] wr_data,  
 						input logic wr_en, 
-						input clk; 
+						input clk,  
 						output logic [31:0] read_out_1, read_out_2);
 	
 	//need to implement register? 
-	logic [31:0][31:0] register;
+	// logic [31:0] [31:0] regsiter 
+	logic [31:0] register [31:0] ;
+	
+	assign register[2] = 32'd20;
+	assign register[4] = 32'd40; 
+	assign register[8] = 32'd80; 
 
 	assign read_out_1 = register[read_reg_1];
 	assign read_out_2 = register[read_reg_2];
@@ -25,4 +30,42 @@ module register_file (input logic [4:0] read_reg_1, read_reg_2, wr_reg,
 	end 
 
 endmodule 
+
+module register_file_testbench();
+	logic [4:0] read_reg_1, read_reg_2, wr_reg;
+	logic [31:0] wr_data;  
+	logic wr_en, clk; 
+	logic [31:0] read_out_1, read_out_2;
+	 
+
+	register_file dut (.read_reg_1, .read_reg_2, .wr_reg, .wr_data, .wr_en, .clk, .read_out_1, .read_out_2);
+	
+	
+	// Set up the clock.
+	parameter CLOCK_PERIOD=100;
+	
+	initial begin
+		clk = 1;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+ 
+	 // Set up the inputs to the design. Each line is a clock cycle.
+	initial begin
+		@(posedge clk);
+
+		wr_en <= 0; 
+		@(posedge clk);
+		read_reg_1 <= 4'd2; 
+		read_reg_2 <= 4'd4;
+		wr_reg <= 4'd8; 
+		wr_data <= 32'd16; 
+		repeat(3) @(posedge clk);
+		wr_en <= 1; 
+		repeat(3) @(posedge clk); //read_out_1, read_out_2; 
+
+	 $stop; // End the simulation.
+	 end
+		
+	endmodule  // register_file_testbench
+
 			
