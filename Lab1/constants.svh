91255ec1ba46cbe00d13a22040853a85dbaa260b
Feifan Qiao <qiaofeifan@gmail.com>
2021-02-21 16:39:15 -0800

    Add files via upload

--- Lab1/constants.svh
+++ Lab1/constants.svh
@@ -72,37 +72,37 @@ localparam [2:0]SHU = 3'b101;
 // control signals for alu is defined here
 // 4 operations : 1)op 2) op-imm 3) branch 4) load 5) store
 //for op
-localparam [3:0]ALU_ADD_I	= 4'b0000; // addi (op imm)
-localparam [3:0]ALU_SUB_I	= 4'b0001;
-localparam [3:0]ALU_AND_I	= 4'b0010;
-localparam [3:0]ALU_OR_I	= 4'b0011;
-localparam [3:0]ALU_XOR_I	= 4'b0100;
-localparam [3:0]ALU_SLL_I	= 4'b0101; // shift left logical 
-localparam [3:0]ALU_SRL_I	= 4'b0110;
-localparam [3:0]ALU_SRA_I	= 4'b0111;
+localparam [4:0]ALU_ADD_I	= 5'b00000; // addi (op imm)
+localparam [4:0]ALU_SUB_I	= 5'b00001;
+localparam [4:0]ALU_AND_I	= 5'b00010;
+localparam [4:0]ALU_OR_I	= 5'b00011;
+localparam [4:0]ALU_XOR_I	= 5'b00100;
+localparam [4:0]ALU_SLL_I	= 5'b00101; // shift left logical 
+localparam [4:0]ALU_SRL_I	= 5'b00110;
+localparam [4:0]ALU_SRA_I	= 5'b00111;
 
-localparam [3:0]ALU_BEQ		= 4'b1000;	// branch operations
-localparam [3:0]ALU_BNE		= 4'b1001;
-localparam [3:0]ALU_BLT		= 4'b1010;
-localparam [3:0]ALU_BGE		= 4'b1011;
-localparam [3:0]ALU_BLT_U	= 4'b1100;
-localparam [3:0]ALU_BGE_U	= 4'b1101;
+localparam [4:0]ALU_BEQ		= 5'b01000;	// branch operations
+localparam [4:0]ALU_BNE		= 5'b01001;
+localparam [4:0]ALU_BLT		= 5'b01010;
+localparam [4:0]ALU_BGE		= 5'b01011;
+localparam [4:0]ALU_BLT_U	= 5'b01100;
+localparam [4:0]ALU_BGE_U	= 5'b01101;
 
-localparam [3:0]ALU_SLT_I	= 4'b1110;	// set less than operations
-localparam [3:0]ALU_SLT_I_U = 4'b1111;	
+localparam [4:0]ALU_SLT_I	= 5'b01110;	// set less than operations
+localparam [4:0]ALU_SLT_I_U = 5'b01111;	
+localparam [4:0]ALU_JAL_R	= 5'b10000;
+localparam [4:0]ALU_LUI		= 5'b10001;
+localparam [4:0]ALU_AUIPC	= 5'b10010;
 
 // alu immediate read signals
 localparam [1:0]ALU_READ_RS2		= 2'b00;
 localparam [1:0]ALU_READ_IMM		= 2'b01;
-localparam [1:0]ALU_READ_IMM_U_J	= 2'b10;
+localparam [1:0]ALU_READ_IMM_U		= 2'b10;
+localparam [1:0]ALU_READ_IMM_J		= 2'b11;
 
 // xfer_size
 localparam [2:0]XFER_BYTE 	= 3'b000;
 localparam [2:0]XFER_HALF	= 3'b001;
-<<<<<<< HEAD
-localparam [2:0]XFER_WORD	= 3'b010;
-=======
 localparam [2:0]XFER_WORD	= 3'b010;
 
 
->>>>>>> 87a68024dc4cc71812ce6563ccefe90a5c547843
